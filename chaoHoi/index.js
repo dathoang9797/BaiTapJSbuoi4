61e8ee5c5a2620b41624e2c301553c1e2d157e6d
dathoang9797 <dathoang9797@gmail.com>
2021-12-29 19:07:13 +0700

    First Commit3

--- chaoHoi/index.js
+++ chaoHoi/index.js
@@ -1,14 +1,20 @@
 const outPutResultEl = document.getElementById('outputResult');
 const person = prompt('Who use this machine:', '').toLocaleLowerCase();
 
-if (person === 'bố') {
-  outPutResultEl.innerText = 'Xin chào ' + person;
-} else if (person === 'mẹ') {
-  outPutResultEl.innerText = 'Xin chào ' + person;
-} else if (person === 'anh trai') {
-  outPutResultEl.innerText = 'Xin chào ' + person;
-} else if (person === 'em gái') {
-  outPutResultEl.innerText = 'Xin chào ' + person;
-} else {
-  outPutResultEl.innerText = 'Unknown';
+switch (person) {
+  case 'bố':
+    outPutResultEl.innerText = 'Xin chào ' + person;
+    break;
+  case 'mẹ':
+    outPutResultEl.innerText = 'Xin chào ' + person;
+    break;
+  case 'anh trai':
+    outPutResultEl.innerText = 'Xin chào ' + person;
+    break;
+  case 'em gái':
+    outPutResultEl.innerText = 'Xin chào ' + person;
+    break;
+  default:
+    outPutResultEl.innerText = 'Unknown';
+    break;
 }
