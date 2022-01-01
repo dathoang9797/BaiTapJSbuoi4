3eb30891def00968090dd60557c94f24580b5319
dathoang9797 <dathoang9797@gmail.com>
2022-01-02 04:34:57 +0700

    Update

--- soLeSoChan/index.js
+++ soLeSoChan/index.js
@@ -4,11 +4,6 @@ const soNguyenThuBaEl = document.getElementById('soNguyen3');
 const btnResultEl = document.getElementById('btnResult');
 const outPutResultEl = document.getElementById('outputResult');
 
-/**
- *  Chúng ta sẽ lấy giá trị của 3 số nguyên sau đó so sánh từng lượt 3 giá trị nếu giá trị nào nhỏ hơn 1 thì sẽ bỏ qua tiếp tục so sánh các giá các số nguyên còn lại vì số âm và số 0  vì nó ko phải là chẵn và cungr ko phải lẽ, sau khi thõa mãn điều kiện trên và bắt đầu so sánh nếu số nào chia hết cho 2 là chẵn và ngược lại là lẽ ,sau đó đếm số chẵn lẽ thông qua biến let countChan ,countLe
- *
- **/
-
 btnResultEl.addEventListener('click', function (e) {
   e.preventDefault();
   let countChan = 0;
